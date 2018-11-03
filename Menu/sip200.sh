ccf52985df7c50d40ab161efd29499e8ddaffc5e
jblaine <jblaine>
2018-11-03 10:26:21 -0400

    init

---
+++ Menu/sip200.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+printf "What is your name?  -> "
+read NAME
+
+TRGDIR='./'
+
+func2 () {
+    for i in "$1"/*;
+        do
+
+        if [ -f "$i" ]; then
+            printf "File: %s\n\n" "${i}"
+            sips -Z 200 "${i}";
+        else
+            func2 "${i}"
+        fi
+    done
+    return 0
+}
+
+#####################################
+
+#printf "Input: %s\n\n" "$1"
+
+#find . -type f -name '*.jpg'
+#find . -type f -name '*.jpg' -exec sips -Z 20
+
+#####################################
+
+for i in *;
+    do
+
+    if [ -d "$i" ]; then
+        #printf "File: %s\n\n" "${i}"
+        func2 "${i}"
+    fi
+
+done
+
+
+
+
