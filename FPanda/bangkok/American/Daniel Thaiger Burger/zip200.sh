c8408bdb3041554fd35b6ab9abf830fc280615c8
John Blaine <jblaine@Johns-MacBook-Pro.local>
2018-11-17 14:29:19 -0500

    done

---
+++ FPanda/bangkok/American/Daniel Thaiger Burger/zip200.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+printf "What is your name?  -> "
+read NAME
+
+
+func2 () {
+    for i in "$1"/*;
+        do
+
+        if [ -f "$i" ]; then
+            printf "File: %s\n\n" "${i}"
+#            sips -Z 200 "${i}";
+        else
+            func2 "${i}"
+        fi
+    done
+    return 0
+}
+
+#####################################
+
+for i in *;
+    do
+
+    if [ -d "$i" ]; then
+        #printf "File: %s\n\n" "${i}"
+        func2 "${i}"
+    fi
+
+done
+
+
+
+
